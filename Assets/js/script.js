f03fe6aeb171e40228e16f68c29950c6c7747a21
GK <gareth0201@gmail.com>
2022-10-12 21:23:18 -0400

    Added styles to different pages

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -11,7 +11,7 @@ fetch("https://exercises-by-api-ninjas.p.rapidapi.com/v1/exercises?muscle=chest"
     return response.json();
   })
   .then(function (data) {
-    //console.log(data);
+    console.log(data);
     for (var i = 0; i < data.length; i++) {
       var exerciseTitle = document.createElement("h4");
       exerciseTitle.textContent += data[i].name;
@@ -19,7 +19,7 @@ fetch("https://exercises-by-api-ninjas.p.rapidapi.com/v1/exercises?muscle=chest"
       pEl.textContent += data[i].instructions;
       divEl = document.createElement("div");
       //create styling for this div element
-      //document.getElementById("upperBody").style.padding = "5px";
+      document.getElementById("upperBody").style.padding = "5px";
       divEl.appendChild(exerciseTitle);
       divEl.appendChild(pEl);
       upperBodyEl.appendChild(divEl);
@@ -34,14 +34,14 @@ fetch("https://exercises-by-api-ninjas.p.rapidapi.com/v1/exercises?muscle=middle
     return response.json();
   })
   .then(function (data) {
-    //console.log(data);
+    console.log(data);
     for (var i = 0; i < data.length; i++) {
       var exerciseTitle = document.createElement("h4");
       exerciseTitle.textContent = data[i].name;
       var pEl = document.createElement("p");
-      pEl.textContent += data[i].instructions; 
+      pEl.textContent += data[i].instructions;
       divEl = document.createElement("div");
-      //document.getElementById("upperBody").style.padding = "5px";
+      // document.getElementById("upperBody").style.padding = "5px";
       divEl.appendChild(exerciseTitle);
       divEl.appendChild(pEl);
       upperBodyEl.appendChild(divEl);
@@ -56,15 +56,15 @@ fetch("https://exercises-by-api-ninjas.p.rapidapi.com/v1/exercises?muscle=abdomi
     return response.json();
   })
   .then(function (data) {
-    //console.log(data);
+    console.log(data);
     for (var i = 0; i < data.length; i++) {
       var exerciseTitle = document.createElement("h4");
       exerciseTitle.textContent = data[i].name;
       var pEl = document.createElement("p");
       pEl.textContent += data[i].instructions;
       divEl = document.createElement("div");
-      //Styled div element 
-      //document.getElementById("abs").style.padding = "5px";
+      //Styled div element
+      document.getElementById("abs").style.padding = "5px";
       divEl.appendChild(exerciseTitle);
       divEl.appendChild(pEl);
       absEl.appendChild(divEl);
@@ -73,136 +73,136 @@ fetch("https://exercises-by-api-ninjas.p.rapidapi.com/v1/exercises?muscle=abdomi
     }
   });
 
-  var lowerBodyEl = document.querySelector(".lowerBody");
-  fetch("https://exercises-by-api-ninjas.p.rapidapi.com/v1/exercises?muscle=glutes", options)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      //console.log(data);
-      for (var i = 0; i < data.length; i++) {
-        var exerciseTitle = document.createElement("h4");
-        exerciseTitle.textContent = data[i].name;
-        var pEl = document.createElement("p");
-        pEl.textContent += data[i].instructions;
-        //Styled div element 
-      //document.getElementById("lowerBody").style.padding = "5px";
+var lowerBodyEl = document.querySelector(".lowerBody");
+fetch("https://exercises-by-api-ninjas.p.rapidapi.com/v1/exercises?muscle=glutes", options)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+    for (var i = 0; i < data.length; i++) {
+      var exerciseTitle = document.createElement("h4");
+      exerciseTitle.textContent = data[i].name;
+      var pEl = document.createElement("p");
+      pEl.textContent += data[i].instructions;
+      //Styled div element
+      document.getElementById("lowerBody").style.padding = "5px";
       divEl.appendChild(exerciseTitle);
       divEl.appendChild(pEl);
       lowerBodyEl.appendChild(divEl);
       brEl = document.createElement("br");
       divEl.appendChild(brEl);
-      }
-    });
-  
-    var hamstrings = document.querySelector(".lowerBody");
-    fetch("https://exercises-by-api-ninjas.p.rapidapi.com/v1/exercises?muscle=hamstrings", options)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        //console.log(data);
-        for (var i = 0; i < data.length; i++) {
-          var exerciseTitle = document.createElement("h4");
-          exerciseTitle.textContent = data[i].name;
-          var pEl = document.createElement("p");
-          pEl.textContent += data[i].instructions;
-          //Styled div element 
-        //document.getElementById("lowerBody").style.padding = "5px";
-        divEl.appendChild(exerciseTitle);
-        divEl.appendChild(pEl);
-        hamstrings.appendChild(divEl);
-        brEl = document.createElement("br");
-        divEl.appendChild(brEl);
-        }
-      });
+    }
+  });
 
-      var quadriceps = document.querySelector(".lowerBody");
-      fetch("https://exercises-by-api-ninjas.p.rapidapi.com/v1/exercises?muscle=quadriceps", options)
-        .then(function (response) {
-          return response.json();
-        })
-        .then(function (data) {
-          //console.log(data);
-          for (var i = 0; i < data.length; i++) {
-            var exerciseTitle = document.createElement("h4");
-            exerciseTitle.textContent = data[i].name;
-            var pEl = document.createElement("p");
-            pEl.textContent += data[i].instructions;
-            //Styled div element 
-          //document.getElementById("lowerBody").style.padding = "5px";
-          divEl.appendChild(exerciseTitle);
-          divEl.appendChild(pEl);
-          quadriceps.appendChild(divEl);
-          brEl = document.createElement("br");
-          divEl.appendChild(brEl);
-          }
-        });
+var hamstrings = document.querySelector(".lowerBody");
+fetch("https://exercises-by-api-ninjas.p.rapidapi.com/v1/exercises?muscle=hamstrings", options)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+    for (var i = 0; i < data.length; i++) {
+      var exerciseTitle = document.createElement("h4");
+      exerciseTitle.textContent = data[i].name;
+      var pEl = document.createElement("p");
+      pEl.textContent += data[i].instructions;
+      //Styled div element
+      document.getElementById("lowerBody").style.padding = "5px";
+      divEl.appendChild(exerciseTitle);
+      divEl.appendChild(pEl);
+      hamstrings.appendChild(divEl);
+      brEl = document.createElement("br");
+      divEl.appendChild(brEl);
+    }
+  });
 
-        var cardioEl = document.querySelector(".cardio");
-        fetch("https://exercises-by-api-ninjas.p.rapidapi.com/v1/exercises?type=cardio", options)
-          .then(function (response) {
-            return response.json();
-          })
-          .then(function (data) {
-            //console.log(data);
-            for (var i = 0; i < data.length; i++) {
-              var exerciseTitle = document.createElement("h4");
-              exerciseTitle.textContent += data[i].name;
-              var pEl = document.createElement("p");
-              pEl.textContent += data[i].instructions;
-              divEl = document.createElement("div");
-              //
-              //document.getElementById("cardio").style.padding = "5px";
-              divEl.appendChild(exerciseTitle);
-              divEl.appendChild(pEl);
-              cardioEl.appendChild(divEl);
-              brEl = document.createElement("br");
-              divEl.appendChild(brEl);
-            }
-         });
+var quadriceps = document.querySelector(".lowerBody");
+fetch("https://exercises-by-api-ninjas.p.rapidapi.com/v1/exercises?muscle=quadriceps", options)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+    for (var i = 0; i < data.length; i++) {
+      var exerciseTitle = document.createElement("h4");
+      exerciseTitle.textContent = data[i].name;
+      var pEl = document.createElement("p");
+      pEl.textContent += data[i].instructions;
+      //Styled div element
+      document.getElementById("lowerBody").style.padding = "5px";
+      divEl.appendChild(exerciseTitle);
+      divEl.appendChild(pEl);
+      quadriceps.appendChild(divEl);
+      brEl = document.createElement("br");
+      divEl.appendChild(brEl);
+    }
+  });
 
-         var strengthEl = document.querySelector(".strength");
-  fetch("https://exercises-by-api-ninjas.p.rapidapi.com/v1/exercises?type=strength", options)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      //console.log(data);
-      for (var i = 0; i < data.length; i++) {
-        var exerciseTitle = document.createElement("h4");
-        exerciseTitle.textContent += data[i].name;
-        var pEl = document.createElement("p");
-        pEl.textContent += data[i].instructions;
-        //create styling for this div element
-        //document.getElementById("strength").style.padding = "5px";
-        divEl.appendChild(exerciseTitle);
-        divEl.appendChild(pEl);
-        strengthEl.appendChild(divEl);
-        brEl = document.createElement("br");
-        divEl.appendChild(brEl);
-      }
-    });
-  
-    var stretchingEl = document.querySelector(".stretching");
-    fetch("https://exercises-by-api-ninjas.p.rapidapi.com/v1/exercises?type=stretching", options)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        //console.log(data);
-        for (var i = 0; i < data.length; i++) {
-          var exerciseTitle = document.createElement("h4");
-          exerciseTitle.textContent = data[i].name;
-          var pEl = document.createElement("p");
-          pEl.textContent += data[i].instructions;
-          divEl = document.createElement("div");
-          //Styled div element 
-        //document.getElementById("stretching").style.padding = "5px";
-        divEl.appendChild(exerciseTitle);
-        divEl.appendChild(pEl);
-        stretchingEl.appendChild(divEl);
-        brEl = document.createElement("br");
-        divEl.appendChild(brEl);
-        }
-      });
+var cardioEl = document.querySelector(".cardio");
+fetch("https://exercises-by-api-ninjas.p.rapidapi.com/v1/exercises?type=cardio", options)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+    for (var i = 0; i < data.length; i++) {
+      var exerciseTitle = document.createElement("h4");
+      exerciseTitle.textContent += data[i].name;
+      var pEl = document.createElement("p");
+      pEl.textContent += data[i].instructions;
+      divEl = document.createElement("div");
+      //
+      document.getElementById("cardio").style.padding = "5px";
+      divEl.appendChild(exerciseTitle);
+      divEl.appendChild(pEl);
+      cardioEl.appendChild(divEl);
+      brEl = document.createElement("br");
+      divEl.appendChild(brEl);
+    }
+  });
+
+var strengthEl = document.querySelector(".strength");
+fetch("https://exercises-by-api-ninjas.p.rapidapi.com/v1/exercises?type=strength", options)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+    for (var i = 0; i < data.length; i++) {
+      var exerciseTitle = document.createElement("h4");
+      exerciseTitle.textContent += data[i].name;
+      var pEl = document.createElement("p");
+      pEl.textContent += data[i].instructions;
+      //create styling for this div element
+      document.getElementById("strength").style.padding = "5px";
+      divEl.appendChild(exerciseTitle);
+      divEl.appendChild(pEl);
+      strengthEl.appendChild(divEl);
+      brEl = document.createElement("br");
+      divEl.appendChild(brEl);
+    }
+  });
+
+var stretchingEl = document.querySelector(".stretching");
+fetch("https://exercises-by-api-ninjas.p.rapidapi.com/v1/exercises?type=stretching", options)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+    for (var i = 0; i < data.length; i++) {
+      var exerciseTitle = document.createElement("h4");
+      exerciseTitle.textContent = data[i].name;
+      var pEl = document.createElement("p");
+      pEl.textContent += data[i].instructions;
+      divEl = document.createElement("div");
+      //Styled div element
+      document.getElementById("stretching").style.padding = "5px";
+      divEl.appendChild(exerciseTitle);
+      divEl.appendChild(pEl);
+      stretchingEl.appendChild(divEl);
+      brEl = document.createElement("br");
+      divEl.appendChild(brEl);
+    }
+  });
